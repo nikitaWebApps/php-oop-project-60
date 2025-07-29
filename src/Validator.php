c9de8c4b0bbaa454611d845a2dfe391977d3e7c1
nikitaWebApps <nikitagolovlev.design@gmail.com>
2025-07-29 08:18:52 +0200

    Add main Validator class

---
+++ src/Validator.php
@@ -0,0 +1,12 @@
+<?php
+  namespace Hexlet\Validator;
+
+  use Hexlet\Validator\Schemas\StringSchema;
+
+  class Validator
+  {
+    public function string(): StringSchema
+    {
+      return new StringSchema();
+    }
+  }
